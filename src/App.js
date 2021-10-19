ba2b7aae16f6f81b7278fbe3aa6dc58aa59dc426
Mc Infas <mcinfas9394@gmail.com>
2021-10-19 10:41:27 +0400

    skelton added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      {/* Left Bar */}
+      {/* Right Bar */}
+      {/* Bottom Bar */}
     </div>
   );
 }
